codeaurora-unoffical/platform-external-skia

external/skia: Add QC hooks
Add QC hooks blittering functions

Change-Id: I9d72549ec23b9f5ed69e8337734b32cac165c83d

src/opts/SkBlitRow_opts_arm.cpp
@@ -375,12 +375,24 @@ SkBlitRow::Proc SkBlitRow::PlatformProcs4444(unsigned flags) {
     return SK_ARM_NEON_WRAP(sk_blitrow_platform_4444_procs_arm)[flags];
 }
 
+extern SkBlitRow::Proc32 skia_androidopt_PlatformProcs32(unsigned flags) __attribute__((weak));
+extern SkBlitRow::Proc skia_androidopt_PlatformProcs565(unsigned flags) __attribute__((weak));
+
 SkBlitRow::Proc SkBlitRow::PlatformProcs565(unsigned flags) {
-    return SK_ARM_NEON_WRAP(sk_blitrow_platform_565_procs_arm)[flags];
+    if (skia_androidopt_PlatformProcs565 && skia_androidopt_PlatformProcs565(flags) ) {
+        return  skia_androidopt_PlatformProcs565(flags);
+    } else {
+        return SK_ARM_NEON_WRAP(sk_blitrow_platform_565_procs_arm)[flags];
+    }
 }
 
+
 SkBlitRow::Proc32 SkBlitRow::PlatformProcs32(unsigned flags) {
-    return SK_ARM_NEON_WRAP(sk_blitrow_platform_32_procs_arm)[flags];
+    if (skia_androidopt_PlatformProcs32 && skia_androidopt_PlatformProcs32(flags) ) {
+        return  skia_androidopt_PlatformProcs32(flags);
+    } else {
+        return SK_ARM_NEON_WRAP(sk_blitrow_platform_32_procs_arm)[flags];
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////

OnePlusOSS/android_external_skia

src/opts/SkBlitRow_opts_arm.cpp
@@ -378,8 +378,14 @@ static const SkBlitRow::Proc32 sk_blitrow_platform_32_procs_arm[] = {
 
 #endif // USE_ARM_CODE
 
+extern SkBlitRow::Proc32 skia_androidopt_PlatformProcs32(unsigned flags) __attribute__((weak));
+extern SkBlitRow::Proc16 skia_androidopt_PlatformProcs565(unsigned flags) __attribute__((weak));
 SkBlitRow::Proc16 SkBlitRow::PlatformFactory565(unsigned flags) {
-    return SK_ARM_NEON_WRAP(sk_blitrow_platform_565_procs_arm)[flags];
+    if (skia_androidopt_PlatformProcs565 && skia_androidopt_PlatformProcs565(flags) ) {
+        return  skia_androidopt_PlatformProcs565(flags);
+    } else {
+        return SK_ARM_NEON_WRAP(sk_blitrow_platform_565_procs_arm)[flags];
+    }
 }
 
 SkBlitRow::ColorProc16 SkBlitRow::PlatformColorFactory565(unsigned flags) {
@@ -387,6 +393,10 @@ SkBlitRow::ColorProc16 SkBlitRow::PlatformColorFactory565(unsigned flags) {
 }
 
 SkBlitRow::Proc32 SkBlitRow::PlatformProcs32(unsigned flags) {
-    return SK_ARM_NEON_WRAP(sk_blitrow_platform_32_procs_arm)[flags];
+    if (skia_androidopt_PlatformProcs32 && skia_androidopt_PlatformProcs32(flags) ) {
+        return  skia_androidopt_PlatformProcs32(flags);
+    } else {
+        return SK_ARM_NEON_WRAP(sk_blitrow_platform_32_procs_arm)[flags];
+    }
 }
 